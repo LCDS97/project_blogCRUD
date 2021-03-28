cefe602c856e497f24652a3f509e15fbdd52ea7f
LCDS97 <lucas.cds1997@gmail.com>
2021-03-28 03:17:58 -0300

    Criado método de sessão do projeto
    Criado rota de sessão e página de login para autenticação de usuário

--- users/UsersController.js
+++ users/UsersController.js
@@ -40,4 +40,35 @@ router.post("/users/create", (req, res) => {
 
 });
 
+// Rota de login de adminstrador
+router.get("/login", (req, res) => {
+    res.render("admin/users/login")
+});
+
+// Autenticação de admin
+router.post("/authenticate", (req,res) => {
+
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({where:{ email:email }}).then(user => {
+        if(user != undefined){// Caso exista um usuário
+            // Validando senha
+            var correctPass = bcrypt.compareSync(password,user.password);
+            if(correctPass){ // Caso a senha esteja correta, é criado a sessão de login
+                req.session.user = {
+                    id: user.id,
+                    email: user.email
+                }
+                res.json(req.session.user);
+            }else{
+                res.redirect("/login");
+            }
+
+        }else{// Criar uma mensagem de tratativa de usuário não encontrado
+            res.redirect("/login");
+        }
+    });
+});
+
 module.exports = router;
